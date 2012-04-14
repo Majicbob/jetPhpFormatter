023c41fa4299ee9fd6f5193c3d1999740863af37
John Tribolet <john@tribolet.info>
2012-04-14 14:36:49 -0400

    Tests for Token class, test for construct with item from token_get_all

---
+++ tests/Token.class.Test.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Token class unit tests 
+ * 
+ * @package  Tests
+ * @author   John Tribolet <john@tribolet.info>
+ * @filesource
+ */
+
+use jet\Formatter\Token as Token;
+
+class TokenClassTest extends PHPUnit_Framework_TestCase
+{
+    public function testAutoloaderWorks()
+    {
+        $t = new Token(null);
+    }
+    
+    public function testConstructWithItemFromGetAllMethod()
+    {
+        $tokens = token_get_all('<?php');
+        $t = new Token($tokens[0]);
+        $this->assertTrue(!empty($t->name));
+        $this->assertEquals('T_OPEN_TAG', $t->name);
+    }
+}

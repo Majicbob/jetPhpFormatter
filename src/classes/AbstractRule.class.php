95a78608cc1136e631752e4b7b3bd8326e1f574a
John Tribolet <john@tribolet.info>
2012-04-19 00:06:22 -0400

    New abstract rule class, name, description and apply()

---
+++ src/classes/AbstractRule.class.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Abstract Rule Class 
+ *
+ * Provides the basic setup for a formatting rule 
+ *
+ * @package  jetPhpFormatter
+ * @author   John Tribolet <john@tribolet.info>
+ * @version  0.0.2
+ * @since    22:04 Wednesday, April 18, 2012
+ * @filesource
+ */
+
+
+namespace jet\Formatter;
+
+abstract class AbstractRule
+{
+    /**
+     * @var string Friendly short name
+     */
+    public $name;
+
+    /**
+     * @var string Description of the rule
+     */
+    public $description;
+
+    /**
+     * Takes an array of Tokens and applys the rule 
+     * 
+     * @returns array Tokens with rule applied 
+     */
+    abstract public function apply($tokens) {}
+    
+}
